a3dc76fd41f7f04ea75d84801842ae13388968bc
Hugo-Ap <araparhug7@gmail.com>
2023-10-05 19:03:18 -0500

    Update auth provider

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+/* ********************************** Login ********************************* */
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+/* ****************************** Registration ****************************** */
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+/* *************************** Generic Exceptions *************************** */
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
