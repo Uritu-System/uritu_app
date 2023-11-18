8331dc8ca668739f7e4fffb5cc33ebe4939bfa71
Hugo-Ap <araparhug7@gmail.com>
2023-11-17 22:12:45 -0500

    update

--- lib/common/constants/stt_quechua_url.dart
+++ lib/common/constants/stt_quechua_url.dart
@@ -1,1 +1,1 @@
-const sttQuechuaUrl = "http://34.71.36.238:5000/predictions";
+const sttQuechuaUrl = "";
--- lib/common/constants/translator_key.dart
+++ lib/common/constants/translator_key.dart
@@ -1,3 +1,3 @@
-const cloudTranslationApiKey = 'AIzaSyBREdPiXiX2AF5uaGGz34U9517aaWW_kbU';
+const cloudTranslationApiKey = '';
 const quechuaCodeTranslation = 'qu';
 const spanishCodeTranslation = 'es';
